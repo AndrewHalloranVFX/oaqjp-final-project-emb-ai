5aea09fbfe96161d79535b9c133200301cc1cf27
AndrewHalloranVFX <andrewhalloranvfx@gmail.com>
2024-11-09 14:05:46 -0500

    Adding in server file

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, json, jsonify
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector", methods=["GET"])
+def sent_detector():
+    
+    text_to_analyze = request.args.get("textToAnalyze", "")
+
+    emotions = emotion_detector(text_to_analyze)
+
+    return jsonify(emotions)
+    return jsonify(dominant_emotion)
+
+    if not text_to_analyze:
+        return jsonify({"error": "No text provided"}), 400
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
