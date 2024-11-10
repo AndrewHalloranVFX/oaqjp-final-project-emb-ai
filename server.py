69aa612d2182aa9b9612b6d02d930b5aec2e3ff2
AndrewHalloranVFX <andrewhalloranvfx@gmail.com>
2024-11-10 13:29:39 -0500

    Final Updates

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Server for Emotion Detection
+"""
+
 from flask import Flask, render_template, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,18 +9,28 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector", methods=["GET"])
 def sent_detector():
-    
+
+    """
+    Endpoint for detecting emotions in text.
+    Takes a text input from user and returns a JSON response
+    with emotion scores and the dominant emotion
+    """
+
     text_to_analyze = request.args.get("textToAnalyze", "")
-
-    emotions = emotion_detector(text_to_analyze)
-
-    return jsonify(emotions)
 
     if not text_to_analyze:
         return jsonify({"error": "Invalid Text! Please Try Again"}), 400
 
+    emotions = emotion_detector(text_to_analyze)
+    return jsonify(emotions)
+
 @app.route("/")
 def render_index_page():
+
+    """
+    Renders Index Page
+    """
+
     return render_template('index.html')
 
 if __name__ == "__main__":
